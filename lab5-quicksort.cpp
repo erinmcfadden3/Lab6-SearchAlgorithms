0f75a71a9cb901f9f2ea72294c917aa128a9da22
Erin McFadden <erbo.mcfadden@gmail.com>
2025-11-21 12:20:20 -0500

    Add files via upload

--- lab5-quicksort.cpp
+++ lab5-quicksort.cpp
@@ -1,61 +1,47 @@
-//Erin McFadden
-//Lab 5
-//Quicksort Algorithm
-
 #include <iostream>
-using namespace std;
+using namespace std
 
-long medianOfThree(long arr[], int low, int high) {
-    int mid = low + (high - low) / 2;
-
-    if (arr[low] > arr[mid]) std::swap(arr[low], arr[mid]);
-    if (arr[mid] > arr[high]) std::swap(arr[mid], arr[high]);
-    if (arr[low] > arr[mid]) std::swap(arr[low], arr[mid]);
-
-    return arr[mid]; // the median as pivot
+void quickSort(long int arr[], int first, int last) {
+	if (first >= last) {  //base case
+		return;
+	}
+	int lower = first + 1;
+	int upper = last;
+	swap(arr[first], arr[(first + last)/2]);
+	int bound = arr[first];
+	while(lower <= upper) {
+		while (bound > arr[lower]) {
+			lower++;
+		}
+		while (bound < arr[upper]) {
+			upper--;
+		}
+		if (lower < upper) {
+			swap(arr[lower++], arr[upper--]);
+		} else {
+			lower++;
+		}
+	}
+	swap(arr[upper], arr[first]);
+	if (first < upper- 1) {
+		quickSort(arr, first, upper - 1);
+	}
+	if (upper + 1 < last) {
+		quickSort(arr, upper + 1, last);
+	}
 }
 
-int partition(long arr[], int low, int high) {
-    long pivot = medianOfThree(arr, low, high);
-
-    int i = low - 1;
-    int j = high + 1;
-
-    while (true) {
-        do { i++; } while (arr[i] < pivot);
-        do { j--; } while (arr[j] > pivot);
-
-        if (i >= j) return j;
-
-        swap(arr[i], arr[j]);
-    }
+void quickSort(long int arr[], int n){
+	int i, max;
+	if (n < 2) {
+		return;
+	}
+	for (i = 1; max = 0; i < n; i++) {
+		if (arr[max] < arr[i]) {
+			max = i;
+		}
+	}
+	swap(arr[n-1], arr[max]);
+	quickSort(arr, 0, n -2);
 }
 
-void quickSortRec(long arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-
-        quickSortRec(arr, low, pi);
-        quickSortRec(arr, pi + 1, high);
-    }
-}
-
-void quickSort(long arr[], int size) {
-    quickSortRec(arr, 0, size - 1);
-}
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-}
